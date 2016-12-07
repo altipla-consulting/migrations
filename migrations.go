9a1d39d4133d75802d800b1cea098e9dff37f36e
Ernesto Alejo <ernestokarim@gmail.com>
2016-12-07 23:25:34 +0100

    WIP: Use a simpler interface to SQL.

--- migrations.go
+++ migrations.go
@@ -2,49 +2,47 @@ package migrations
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"time"
+	"database/sql"
 
+	"github.com/Sirupsen/logrus"
 	"github.com/altipla-consulting/schema"
 	"github.com/altipla-consulting/schema/column"
-	"github.com/jmoiron/sqlx"
 	"github.com/juju/errors"
 )
 
-// Logger that will be used to show the progress of the migrations. It can be
-// overriden by test to output nothing.
-var Logger = log.New(os.Stderr, "", log.LstdFlags)
+const tableMigrations = "migrations"
 
 type M struct {
 	Name  string
-	Apply func(db *sqlx.DB, conn *schema.Connection) error
+	Apply func(db *sql.DB, conn *schema.Connection) error
 }
 
-type AppliedMigration struct {
+type appliedMigration struct {
 	Name     string
 	RunnedAt time.Time
 }
 
-func Run(db *sqlx.DB, migrations []M) error {
-	return errors.Trace(RunConnection(db, schema.NewConnection(db.DB), migrations))
+func Run(db *sql.DB, migrations []M) error {
+	return errors.Trace(RunConnection(db, schema.NewConnection(db), migrations))
 }
 
-func RunConnection(db *sqlx.DB, conn *schema.Connection, migrations []M) error {
-	Logger.Println("--- Migrations found:", len(migrations))
+func RunConnection(db *sql.DB, conn *schema.Connection, migrations []M) error {
+	logrus.WithFields(logrus.Fields{"number": len(migrations)}).Info("run migrations")
 
-	Logger.Println("--- Check migrations table")
 	columns := []schema.Column{
 		column.String("name", 191).PrimaryKey(),
 		column.DateTime("runned_at").DefaultCurrent(),
 	}
-	if err := conn.CreateTableIfNotExists("migrations", columns); err != nil {
+	if err := conn.CreateTableIfNotExists(tableMigrations, columns); err != nil {
 		return errors.Trace(err)
 	}
 
-	stored := []*AppliedMigration{}
-	if err := db.Select(&stored, `SELECT * FROM migrations ORDER BY runned_at`); err != nil {
-		return errors.Trace(err)
+	collection := dbcollections.Table(tableMigrations)
+	stored := []*appliedMigration{}
+	if err := collection.OrderBy("runned_at").All(&stored); err != nil {
+	  return errors.Trace(err)
 	}
 	applied := map[string]bool{}
 	for _, m := range stored {
@@ -54,17 +52,17 @@ func RunConnection(db *sqlx.DB, conn *schema.Connection, migrations []M) error {
 	for i, migration := range migrations {
 		name := fmt.Sprintf("%03d_%s", i, migration.Name)
 		if !applied[name] {
-			Logger.Println("--- Apply migration:", name)
+			logrus.WithFields(logrus.Fields{"name": name}).Info("apply migration")
 			if err := migration.Apply(db, conn); err != nil {
 				return errors.Annotatef(err, "migration %s failed", name)
 			}
 
-			if _, err := db.Exec(`INSERT INTO migrations(name) VALUES (?)`, name); err != nil {
-				return errors.Trace(err)
+			if err := collection.Insert(&appliedMigration{Name: name}); err != nil {
+			  return errors.Trace(err)
 			}
 		}
 	}
 
-	Logger.Println("--- Migrations applied successfully!")
+	logrus.Info("migrations applied successfully")
 	return nil
 }
